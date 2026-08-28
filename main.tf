ce6b2fee572b05c2e9eb5c2ec2ec863c1e787df5
divya <div.tha@gmail.com>
2026-08-28 21:23:14 +0530

    fix: node type chnaged to small

--- main.tf
+++ main.tf
@@ -23,7 +23,9 @@ resource "google_container_node_pool" "primary_nodes" {
   node_count = 2
 
   node_config {
-    machine_type = "e2-medium"
+    machine_type = "e2-small"
+    disk_size_gb = 20 
+    disk_type = "pd-standard"
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
